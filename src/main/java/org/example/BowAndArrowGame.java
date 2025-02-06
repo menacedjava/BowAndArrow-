25472e5d9d208035892bd74ef433e29060a7c957
Madumarov Davron <menaceteacher@gmail.com>
2025-02-07 02:39:09 +0500

    add

---
+++ src/main/java/org/example/BowAndArrowGame.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+
+public class BowAndArrowGame extends JPanel implements ActionListener, KeyListener {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
